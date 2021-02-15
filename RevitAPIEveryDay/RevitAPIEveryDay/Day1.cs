8db2eaf9a45f0f791de65eb89d4a8242d3926e29
chuongmep <chuongpqvn@gmail.com>
2021-02-15 20:17:31 +0700

    init

---
+++ RevitAPIEveryDay/RevitAPIEveryDay/Day1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitAPIEveryDay
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    [Journaling(JournalingMode.UsingCommandData)]
+    public class Day1 : IExternalCommand
+    {
+        /// <summary>
+        /// Hello Word
+        /// </summary>
+        /// <param name="commandData"></param>
+        /// <param name="message"></param>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Notification", "Hello Word", TaskDialogCommonButtons.Ok);
+            return Result.Succeeded;
+        }
+    }
+}
