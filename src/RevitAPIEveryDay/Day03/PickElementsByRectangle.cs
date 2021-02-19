0666cc3f6c88c7830713858f4f5f14465600c928
chuongmep <31106432+chuongmep@users.noreply.github.com>
2021-02-19 15:53:42 +0700

    PickElementsByRectangle

---
+++ src/RevitAPIEveryDay/Day03/PickElementsByRectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+
+namespace Day03
+{
+    [Transaction(TransactionMode.Manual)]
+    public class PickElementsByRectangle : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+            IList<Element> eles = uidoc.Selection.PickElementsByRectangle(new SelectionFilter(), "Select Element By Drag Mouse Rectangle");
+            MessageBox.Show(eles.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+    public class SelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem !=null) return true;
+            return false;
+           
+        }
+        public bool AllowReference(Reference refer, XYZ pos)
+        {
+            return false;
+           
+        }
+    }
+}
