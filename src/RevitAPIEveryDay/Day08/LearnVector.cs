9584d8a073cdd461f36976fe109cfd0e174f8ce1
chuongmep <31106432+chuongmep@users.noreply.github.com>
2021-05-21 23:23:21 +0700

    Update LearnVector.cs

--- src/RevitAPIEveryDay/Day08/LearnVector.cs
+++ src/RevitAPIEveryDay/Day08/LearnVector.cs
@@ -19,7 +19,7 @@ namespace LearnAPI.Day08
             XYZ u = XYZ.BasisX;
             XYZ v = XYZ.BasisY;
             XYZ w = XYZ.BasisZ;
-            
+
             //dot product : Angle Between Vector
             double dotProduct = u.DotProduct(v); // cos90 = 0
             MessageBox.Show($"Angle : {dotProduct}");
@@ -32,21 +32,25 @@ namespace LearnAPI.Day08
 
             //This article is about ternary operations on vectors. For the identity in number theory, see Jacobi triple product.
             // https://en.wikipedia.org/wiki/Triple_product
-            double tripleProduct = u.TripleProduct(v,w);
+            //(u,v,w)=u⋅(v×w)=v⋅(w×u)=w⋅(u×v)
+            //(u,v,w)=(w,u,v)=(v,w,u)=−(v,u,w)=−(w,v,u)=−(u,w,v)
+            // ku⋅(v×w)=k(u,v,w)
+            //(u,v,w)=u⋅(v×w)
+            double tripleProduct = u.TripleProduct(v, w);
             System.Windows.Forms.MessageBox.Show($"{nameof(tripleProduct)}: {tripleProduct}");
 
             // Write By Step By Step 
             //Dot Product
-            DotProduct(u,v).ShowMessageBox();
+            DotProduct(u, v).ShowMessageBox();
             //Cross Product
-            CrossProduct(u,v).ShowMessageBox();
+            CrossProduct(u, v).ShowMessageBox();
         }
 
-        public double DotProduct(XYZ u, XYZ v)
+        double DotProduct(XYZ u, XYZ v)
         {
             return u.X * v.X + u.Y * v.Y + u.Z * v.Z;
         }
-        public static XYZ CrossProduct(XYZ u, XYZ v)
+        XYZ CrossProduct(XYZ u, XYZ v)
         {
             double a = u.Y * v.Z - u.Z * v.Y;
             double b = u.Z * v.X - u.X * v.Z;
